807a3469021dcf689ba625fbd0830fc81f4f68cf
rico2290 <rjl220990@gmail.com>
2018-03-31 00:25:48 -0300

    Pequeno projeto de património usando Spring Boot

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into item (etiqueta, descricao, dataaquisicao) values ('NT01234', 'Notebook_i7', sysdate());
+insert into item (etiqueta, descricao, dataaquisicao) values ('AU98755', 'Ford_Fiesta', sysdate());
